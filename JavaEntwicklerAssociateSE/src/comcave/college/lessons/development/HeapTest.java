97d9170abde259a75e7daf49083cb38a0fcde8cc
mamiri <mamiri@PC05160760.cc-college.gecs.loc>
2019-02-07 13:01:33 +0100

    versuch1

--- JavaEntwicklerAssociateSE/src/comcave/college/lessons/development/HeapTest.java
+++ JavaEntwicklerAssociateSE/src/comcave/college/lessons/development/HeapTest.java
@@ -5,10 +5,60 @@
  */
 package comcave.college.lessons.development;
 
+import comcave.college.com.lib.CommonLib;
+import comcave.college.com.lib.utils.ProcessTimeDetection;
+import java.util.HashSet;
+
 /**
  *
  * @author mamiri
  */
 public class HeapTest {
     
+    /*********************************************************************************/
+    /**
+     * 
+     * @param args 
+     * 
+     *  Manipulate the Memory-Sizes Xms32m -Xmx256m (VM-Option)
+     * 
+     * -> Windows-Suche -> cmd <- eingeben
+     * -> java -X <- zeigt Informationen zu diesen Parametern
+     * 
+     * 
+     */
+    public static void main(String[] args) 
+    {
+        ProcessTimeDetection p = new ProcessTimeDetection();
+        System.exit(0);
+        heapTest();
+    }  
+    /*********************************************************************************/
+    
+    private static void heapTest()
+    {
+        System.gc();
+        System.out.println("Start:");
+        CommonLib.printHeapSizeInfoByte();
+        
+        HashSet<String> stack = new HashSet<String>();
+        
+        for (int i = 0; i < 5000; i++) 
+        {
+            stack.add(CommonLib.getLeftPaddedString(Integer.toString(i), '0', 8));
+        }
+        
+        System.out.println("Stack loaded:");
+        CommonLib.printHeapSizeInfoByte();
+        
+        stack.clear();
+        stack = null;
+        
+        System.gc();
+        System.out.println("Stack cleared:");
+        CommonLib.printHeapSizeInfoByte();
+    }
+    
+    /*********************************************************************************/
+        
 }
